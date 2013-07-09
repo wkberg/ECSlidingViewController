3d7034e59d2c74acaa91de5491d60b8099d18fab
Wouter <piet@wkberg.nl>
2013-07-09 21:43:54 +0200

    Update InitialSlidingViewController.m

--- ECSlidingViewController/InitialSlidingViewController.m
+++ ECSlidingViewController/InitialSlidingViewController.m
@@ -5,7 +5,7 @@
 //  Created by Michael Enriquez on 1/25/12.
 //  Copyright (c) 2012 EdgeCase. All rights reserved.
 //
-//  Edited by wkberg on 6/8/12.
+//  Edited by wkberg on 6/8/13.
 
 #import "InitialSlidingViewController.h"
 
@@ -27,11 +27,19 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   
+  UIStoryboard *storyboard;
+  
+  if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+    storyboard = [UIStoryboard storyboardWithName:@"iPhone" bundle:nil];
+  } else if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+    storyboard = [UIStoryboard storyboardWithName:@"iPad" bundle:nil];
+  }
+  
   
   NSString *identifier = @"FirstTop"; //This is the ident for the first top viewcontroller
     UIViewController *initViewController = [ECSlidingViewController queryViewController:identifier]; //Check if viewcontroller does exist
     if (!initViewController) {
-        initViewController = [self.storyboard instantiateViewControllerWithIdentifier:identifier]; //if not: instantiate it
+        initViewController = [storyboard instantiateViewControllerWithIdentifier:identifier]; //if not: instantiate it
         [ECSlidingViewController addViewController:identifier viewController:initViewController]; //add instantiated vC to the list
     }
     self.topViewController = initViewController;
