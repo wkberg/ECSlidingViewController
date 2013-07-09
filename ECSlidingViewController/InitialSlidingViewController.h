d27554a62d7553ca8fd8499c2ab1f7e46253bffe
Wouter <piet@wkberg.nl>
2013-07-09 21:45:00 +0200

    Update InitialSlidingViewController.h

--- ECSlidingViewController/InitialSlidingViewController.h
+++ ECSlidingViewController/InitialSlidingViewController.h
@@ -5,7 +5,7 @@
 //  Created by Michael Enriquez on 1/25/12.
 //  Copyright (c) 2012 EdgeCase. All rights reserved.
 //
-//  Edited by wkberg on 6/8/12.
+//  Edited by wkberg on 6/8/13
 
 #import "ECSlidingViewController.h"
 
