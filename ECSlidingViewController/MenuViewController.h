ca3c815b30cab6b44781707dfd7d4f8863de0822
Wouter <piet@wkberg.nl>
2013-07-09 21:40:28 +0200

    Update MenuViewController.h

--- ECSlidingViewController/MenuViewController.h
+++ ECSlidingViewController/MenuViewController.h
@@ -5,7 +5,7 @@
 //  Created by Michael Enriquez on 1/23/12.
 //  Copyright (c) 2012 EdgeCase. All rights reserved.
 //
-//  Edited by wkberg on 6/8/12
+//  Edited by wkberg on 6/8/13
 
 #import <UIKit/UIKit.h>
 #import "ECSlidingViewController.h"
